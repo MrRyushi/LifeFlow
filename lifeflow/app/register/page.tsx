d387a1566a72b4b3ece7af93a591627e30685e10
MrRyushi <jptmarcellana@gmail.com>
2026-01-16 22:16:07 +0800

    added roles when registering as a donor

--- lifeflow/app/register/page.tsx
+++ lifeflow/app/register/page.tsx
@@ -1,47 +1,68 @@
-"use client"
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from '@/firebase/firebase'
+import { auth, db } from "@/firebase/firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-  const handleSubmit = (event: React.FormEvent) =>{
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
- 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed up 
-        const user = userCredential.user;
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
-  }
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+
+      await setDoc(
+        doc(db, "users", user.uid),
+        { role: "donor" },
+        { merge: true }
+      );
+
+      alert("Registration Successful");
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.error(error.code, error.message);
+      alert(error.message);
+    }
+  };
 
   return (
     <div>
-      <h1>Register Page</h1>
-      <p>Please fill in the form to create an account.</p>
+      <h1>Donor Sign Up</h1>
+      <p>Please fill in the form to create a donor account.</p>
       <form onSubmit={handleSubmit}>
         <label>
           Email:
-          <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <br />
         <label>
           Password:
-          <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <br />
         <button type="submit">Register</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
